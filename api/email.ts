f2544df8fae051ef2de87aea919a4264d5d1c2b4
Cabeda <jecabeda@gmail.com>
2021-11-05 15:06:26 +0000

    Rename api files

---
+++ api/email.ts
@@ -0,0 +1,24 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import sgMail from "@sendgrid/mail";
+export default (_req: VercelRequest, res: VercelResponse) => {
+  // using Twilio SendGrid's v3 Node.js Library
+  // https://github.com/sendgrid/sendgrid-nodejs
+  
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: "test@example.com", // Change to your recipient
+    from: "test@example.com", // Change to your verified sender
+    subject: "Sending with SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+      res.status(200).send("Email sent");
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+};
