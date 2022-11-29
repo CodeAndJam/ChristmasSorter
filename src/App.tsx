d83b950ec3d7fad8dcb58dc1726621950e239015
José Cabeda <jecabeda@gmail.com>
2022-11-29 21:26:59 +0000

    Update create-react-app and migrate to react hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,15 @@
 import React from "react";
 import "./css/App.css";
 import RegistrationForm from "./Components/RegistrationForm";
-import { SorterEvent } from "../Shared/Interfaces/SorterEventAPI.Interface";
 
-export default class App extends React.Component {
-  registerUser = ({
-    serialized,
-    fields,
-    form,
-  }: {
-    serialized: any;
-    fields: any;
-    form: any;
-  }) => {
-    const body: SorterEvent = {
-      name: fields.eventName.value,
-      members: JSON.parse(fields.participants.value),
-      date: fields.eventDate.value,
-      giftPrice: fields.price.value,
-      currency: fields.currency.value,
-    };
-
-    return fetch(`/api/email`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json; charset=utf-8",
-      },
-      body: JSON.stringify(body),
-    })
-      .then(() => alert("Sent with success"))
-      .catch((e) => alert("Error"));
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Christmas Sorter</h1>
-        </header>
-        <RegistrationForm action={this.registerUser} />
-      </div>
-    );
-  }
+export default function App() {
+ 
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Christmas Sorter</h1>
+      </header>
+      <RegistrationForm />
+    </div>
+  );
 }
